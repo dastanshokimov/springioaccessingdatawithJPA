c663c1e354971942dad50c231f0ddbf03f4014dd
dastanshokimov <dastan.shokimov@gmail.com>
2022-07-26 17:54:16 +0600

    First Commit

---
+++ src/main/java/com/example/springio_accessing_data_with_jpa/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.springio_accessing_data_with_jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
---
+++ src/test/java/com/example/springio_accessing_data_with_jpa/SpringioAccessingDataWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springio_accessing_data_with_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringioAccessingDataWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
